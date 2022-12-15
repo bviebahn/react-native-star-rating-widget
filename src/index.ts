194dfd5962f6e3ff023a66ebd8fb82772569b5b4
Benedikt Viebahn <benedikt.viebahn@live-eo.com>
2022-12-15 11:41:25 +0100

    fix: change StarIconProps type export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import StarRating from "./StarRating";
 export { default as StarRatingDisplay } from "./StarRatingDisplay";
-export { default as StarIcon, StarIconProps } from "./StarIcon";
+export { default as StarIcon } from "./StarIcon";
+export type { StarIconProps } from "./StarIcon";
 
 export default StarRating;
