ac65ba9bb52facc6ec2b191b2f855dd9d14d8c09
Benedikt Viebahn <benedikt.viebahn@live-eo.com>
2022-12-16 13:53:52 +0100

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import StarRating from "./StarRating";
-export { default as StarRatingDisplay } from "./StarRatingDisplay";
-export { default as StarIcon } from "./StarIcon";
-export type { StarIconProps } from "./StarIcon";
+import StarRating from './StarRating';
+export { default as StarRatingDisplay } from './StarRatingDisplay';
+export { default as StarIcon } from './StarIcon';
+export type { StarIconProps } from './StarIcon';
 
 export default StarRating;
